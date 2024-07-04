3e547f9df805d44f3a552bb69c63678850629ea7
hendrixperalta <hendrixperalta@gmail.com>
2024-07-04 19:41:18 +0900

    Modified the predictors for Index 3
    Score increased by 6 point
    
     Eliminated 'lnagr_land2012' 'ln_land_temp2012' 'ln_dist_road2017' 'ln_dist_drug2017mean' 'Chuquisaca' 'Cochabamba'
    # 'La Paz' 'photov2019mean' 'Beni' 'ln_precCRU2012min' 'Oruro'2' 'Chuquisaca' 'Tarija' 'lnurb_land2012' 'Santa Cruz' 'Pando''ln_dist_drug2017mean' 'ln_land_temp2012''Cochabamba' 'ln_dist_road2017'

--- 02_ridge_model.py
+++ 02_ridge_model.py
@@ -139,9 +139,9 @@ X_index_2 = ['ln_ghsl2015', 'lnurb_land2012','ln_land_temp2012', 'ln_tr400_pop20
                         'Oruro', 'Pando', 'Potosí', 'Santa Cruz', 'Tarija', 'ln_precCRU2012min', 'ln_elev2017mean']
 #       Eliminated 'Beni' 'lnagr_land2012' 'La Paz'
 
-X_index_3 = ['ln_ghsl2015', 'lnagr_land2012', 'lnurb_land2012','ln_land_temp2012', 'ln_tr400_pop2012', 'ln_dist_road2017',
-                        'ln_dist_drug2017mean', 'ln_pm25_2012', 'photov2019mean', 'Beni', 'Chuquisaca', 'Cochabamba', 'La Paz',
-                        'Oruro', 'Pando', 'Potosí', 'Santa Cruz', 'Tarija', 'ln_precCRU2012min']
+X_index_3 = ['ln_ghsl2015', 'lnurb_land2012', 'ln_tr400_pop2012', 'ln_pm25_2012', 'Pando', 'Potosí', 'Santa Cruz', 'Tarija']
+#       Eliminated 'lnagr_land2012' 'ln_land_temp2012' 'ln_dist_road2017' 'ln_dist_drug2017mean' 'Chuquisaca' 'Cochabamba' 
+# 'La Paz' 'photov2019mean' 'Beni' 'ln_precCRU2012min' 'Oruro'
 
 X_index_4 = ['ln_ghsl2015', 'lnagr_land2012', 'lnurb_land2012', 'ln_tr400_pop2012','ln_dist_drug2017mean', 
              'photov2019mean', 'Beni', 'Chuquisaca', 'La Paz', 'Oruro', 'Pando', 'Potosí', 'Santa Cruz', 

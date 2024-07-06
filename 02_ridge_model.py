7729ff11fde9a2bd5ce133881278ee8fa8e36e68
hendrixperalta <hendrixperalta@gmail.com>
2024-07-06 18:14:02 +0900

    Changed predictors Index 13 - added Ground slope
    The score increased by 0.2%

--- 02_ridge_model.py
+++ 02_ridge_model.py
@@ -168,30 +168,32 @@ X_index_3 = [e for e in X if e not in erase_x3]
 #        * Should add NTL later again in increased the score slightly but was irrelevant
 
 erase_x4 = ['Cochabamba', 'ln_dist_road2017', 'ln_elev2017mean', 'ln_land_temp2012', 'ln_pm25_2012', 'ln_t400NTLpc2012',
-            'access2016mean']
+            'access2016mean', "ln_slope500m2017mean"]
 X_index_4 = [e for e in X if e not in erase_x4]
 
 erase_x5 = ['Chuquisaca', 'Pando', 'Santa Cruz', 'Tarija', 'ln_t400NTLpc2012', 'ln_tr400_pop2012', 'lnurb_land2012',
-            'access2016mean']
+            'access2016mean', "ln_slope500m2017mean"]
 X_index_5 = [e for e in X if e not in erase_x5]
 #        * Should add NTL later again in increased the score slightly but was irrelevant
 
 erase_x6 = ['Chuquisaca', 'Cochabamba', 'La Paz', 'Pando', 'ln_dist_drug2017mean', 'ln_elev2017mean', 'ln_land_temp2012', 
-            'ln_precCRU2012min','access2016mean']
+            'ln_precCRU2012min','access2016mean', "ln_slope500m2017mean"]
 X_index_6 = [e for e in X if e not in erase_x6]
 
-erase_x7 = ['La Paz', 'Oruro', 'ln_land_temp2012', 'ln_precCRU2012min', 'photov2019mean','access2016mean']
+erase_x7 = ['La Paz', 'Oruro', 'ln_land_temp2012', 'ln_precCRU2012min', 'photov2019mean','access2016mean', 
+            "ln_slope500m2017mean"]
 X_index_7 = [e for e in X if e not in erase_x7]
 
 erase_x8 = ['Cochabamba', 'Oruro', 'Potosí', 'ln_dist_road2017', 'ln_elev2017mean','ln_t400NTLpc2012', 'ln_tr400_pop2012', 
             'photov2019mean', 'access2016mean']
 X_index_8 = [e for e in X if e not in erase_x8]
 
-erase_x9 = ['Beni', 'Potosí', 'Santa Cruz', 'ln_land_temp2012', 'ln_precCRU2012min', 'lnagr_land2012', 'access2016mean']
+erase_x9 = ['Beni', 'Potosí', 'Santa Cruz', 'ln_land_temp2012', 'ln_precCRU2012min', 'lnagr_land2012', 'access2016mean',
+            "ln_slope500m2017mean"]
 X_index_9 = [e for e in X if e not in erase_x9]
 
 erase_x10 = ['Santa Cruz', 'ln_dist_drug2017mean', 'ln_dist_road2017', 'ln_land_temp2012', 'ln_pm25_2012', 
-             'ln_t400NTLpc2012', 'access2016mean']
+             'ln_t400NTLpc2012', 'access2016mean',"ln_slope500m2017mean"]
 X_index_10 = [e for e in X if e not in erase_x10]
 #        * Should add NTL later again in increased the score slightly but was irrelevant
 
@@ -210,15 +212,15 @@ X_index_15 = [e for e in X if e not in erase_x15]
 
 erase_x16 = ['Oruro', 'Pando', 'Potosí', 'Santa Cruz', 'ln_dist_drug2017mean', 'ln_elev2017mean', 'ln_ghsl2015', 
              'ln_land_temp2012','ln_pm25_2012', 'ln_precCRU2012min', 'ln_t400NTLpc2012', 'ln_tr400_pop2012', 
-             'photov2019mean','access2016mean']
+             'photov2019mean','access2016mean', "ln_slope500m2017mean"]
 X_index_16 = [e for e in X if e not in erase_x16]
 
 erase_x17 = ['Chuquisaca', 'Cochabamba', 'Potosí', 'Santa Cruz','ln_dist_drug2017mean', 'ln_dist_road2017', 'ln_ghsl2015', 
-             'ln_land_temp2012','access2016mean']
+             'ln_land_temp2012','access2016mean', "ln_slope500m2017mean"]
 X_index_17 = [e for e in X if e not in erase_x17]
 
 erase_imds = ['Beni', 'La Paz', 'Oruro', 'Potosí', 'Santa Cruz', 'ln_elev2017mean', 'ln_land_temp2012', 'ln_precCRU2012min',
-               'lnagr_land2012','access2016mean']
+               'lnagr_land2012','access2016mean', "ln_slope500m2017mean"]
 X_imds = [e for e in X if e not in erase_imds]
 
 Xs = [X_index_1, X_index_2, X_index_3, X_index_4, X_index_5, X_index_6, X_index_7, X_index_8, X_index_9, 
@@ -270,6 +272,7 @@ for y_variable, X in zip(y_variables, Xs):
     #opt_scores = evaluate_preds(opt_ri_model, X, y)
     #opt_ridge_results.loc[len(opt_ridge_results.index)] = [y_variable, opt_scores[0], opt_scores[1], 
     #                                                       opt_scores[2]]
+
     #opt_results = opt_ridge_results.round(4).sort_values(by="r2", ascending=False)
     #==================
 

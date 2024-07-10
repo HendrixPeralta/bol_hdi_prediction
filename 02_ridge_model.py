a1e752e0f602f439a11f9490c409ae5b22ca0bb5
hendrixperalta <hendrixperalta@gmail.com>
2024-07-10 20:46:36 +0900

    Changed predictors for SDG11  -'land_per_area_2012_full_forest' did not improve the model

--- 02_ridge_model.py
+++ 02_ridge_model.py
@@ -390,7 +390,8 @@ ridge_predict = sdg10_model.predict(ridge_predict)
 # %% Instance for the SDG 11
 
 erase_x11 = ['Beni', 'Cochabamba', 'Pando','Santa Cruz', 'ln_dist_drug2017mean', 'photov2019mean',
-             'ln_slope500m2017mean', 'ln_elev2017mean', 'ln_access2016mean','ln_t400NTLpc2012', 'ln_pm25_2012']
+             'ln_slope500m2017mean', 'ln_elev2017mean', 'ln_access2016mean','ln_t400NTLpc2012', 'ln_pm25_2012',
+             'land_per_area_2012_full_forest']
 X_index_11 = [e for e in X if e not in erase_x11]
 #        * Should add NTL later again in increased the score slightly but was irrelevant
 

c7c1a893de0e3a5b6c36aefb8be2b004ef107f2a
hendrixperalta <hendrixperalta@gmail.com>
2024-07-10 20:38:11 +0900

    Changed predictors for SDG1  -'land_per_area_2012_full_forest' did not improve the model

--- 02_ridge_model.py
+++ 02_ridge_model.py
@@ -259,7 +259,8 @@ ridge_predict = pd.DataFrame()
 
 # Predictors NOT included in the model 
 erase_x1 = ['Beni', 'La Paz', 'Oruro', 'Pando', 'ln_elev2017mean', 'ln_land_temp2012', 'ln_precCRU2012min', 
-            'ln_dist_drug2017mean', 'ln_slope500m2017mean', 'ln_dist_road2017', 'lnagr_land2012']
+            'ln_dist_drug2017mean', 'ln_slope500m2017mean', 'ln_dist_road2017', 'lnagr_land2012', 
+            'land_per_area_2012_full_forest']
 
 X_index_1 = [e for e in X if e not in erase_x1]
 

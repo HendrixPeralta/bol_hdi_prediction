34deee4201ae078cf069646fb961dcee655b25d4
hendrixperalta <hendrixperalta@gmail.com>
2024-08-27 23:37:17 +0900

    Calculated the Mora's I and P-value of each of the SDG indexes

--- 06_Regional_k-means.py
+++ 06_Regional_k-means.py
@@ -2,12 +2,15 @@
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+import seaborn as sns
 
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
 import libpysal
 from libpysal.weights import Queen, KNN
+
+from esda.moran import Moran
 
 import spopt
 from spopt.region import RegionKMeansHeuristic
@@ -36,6 +39,7 @@ sdg_indexes = ['index_sdg1',
                 'index_sd16',
                 'index_sd17',]
 
+# Quantiles  ======================================================================= 
 fig, axs = plt.subplots(nrows=5, ncols=3, figsize=(30,40))
 axs = axs.flatten()
 
@@ -46,14 +50,13 @@ for i, index in enumerate(sdg_indexes):
         scheme="Quantiles",
         cmap="OrRd",
         edgecolor="k",
-        linewidth=0.2,
+        linewidth=0,
         legend=True,
         ax=ax,
         legend_kwds={
             "fontsize":17,
             "markerscale":1.5,
         }
-        
     )
 
     ax.set_axis_off();
@@ -62,21 +65,35 @@ for i, index in enumerate(sdg_indexes):
 plt.subplots_adjust(wspace=0.1)
 plt.tight_layout()
 plt.show()
-   
-
-
-
-
+# ======================================================================= Quantiles 
+ 
 
 # %%
 # geo_municipalities_w  = libpysal.weights.fuzzy_contiguity(geo_municipalities)
-
-
-
 geo_municipalities_w = Queen.from_dataframe(geo_municipalities)
 
-
 # %%
+np.random.seed(42)
+# Calculate moran's i 
+morans_i_result = [
+    Moran(geo_municipalities[index], geo_municipalities_w) for index in sdg_indexes
+]
+
+# sctructure results as a list of tuples
+morans_i_result=[
+    (index, res.I, res.p_sim)
+    for index, res in zip(sdg_indexes, morans_i_result)
+]
+
+#display table
+# morans_table = pd.DataFrame(
+#     morans_i_result,
+#     columns=["Index", "Moran's I", "P-value"]
+# ).set_index("Index")
+
+
+
+#%%
 kmeans = KMeans(n_clusters=5)
 np.random.seed(42)
 # model = RegionKMeansHeuristic(geo_municipalities['index_sdg1'].values, 5, geo_municipalities_w)
